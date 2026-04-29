d415bf5c0fde630bb7db2a110635dde5101b5107
vinay singh <47773758+vinay2214@users.noreply.github.com>
2026-04-29 16:02:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def generate_ai_reply(user_text):
         """
 
         response = client.models.generate_content(
-            model="gemini-1.5-flash",
+            model="gemini-2.0-flash",
             contents=prompt
         )
 
